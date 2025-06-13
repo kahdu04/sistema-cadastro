c3186f581bd84d8873e4335cb6344335e5eb8e14
kahdu04 <hauracarlos@gmail.com>
2025-06-13 10:04:51 -0300

    Projeto Angular iniciado com Material Design

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatButtonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'crud-angular-material';
+}
